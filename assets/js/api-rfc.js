2155b85010991a629c3e02d80ed5748bf3c589b7
Royce Strange <roycest@swbell.net>
2022-07-09 20:58:56 -0500

    flesh out getRFC function

--- assets/js/api-rfc.js
+++ assets/js/api-rfc.js
@@ -1,9 +1,77 @@
-// TODO: flesh out the function below to get an RFC number, 
-// either a random one if new day/no history, or load it up from history if not a new day.
-// getRFC :: () -> Int
-var getRFC = function () {
-    console.log("todo: getRFC");
-    return 4122;
+var datatracker = "https://datatracker.ietf.org"
+
+var getRFC = async function () {
+    var history = load();
+    var todayRFC = history[0];
+    var today = new Date();
+    var todayFormated = today.getMonth() + "/" + today.getDay() + "/" + today.getFullYear();
+
+    var rfcListUrl = function (offset) {
+        return datatracker + "/api/v1/doc/docevent/" +
+            "?format=" + "json" +
+            "&type=" + "published_rfc" +
+            "&order_by=" + "id" +
+            "&limit=" + 1 +
+            "&offset=" + offset;
+    };
+
+    var rfcDocUrl = function (docPath) {
+        return datatracker + docPath;
+    }
+
+    // if we already have an RFC for today, return that, otherwise go get one.
+    if (todayRFC && todayFormated === todayRFC.date) {
+        return todayRFC.number;
+    }
+    else {
+        var randomRfcIndex = await fetch(rfcListUrl(0))
+            // get json
+            .then(response => {
+                return response.json();
+            })
+            // extract info from json
+            .then(json => {
+                return json.meta.total_count;
+            })
+            // pick a random index 
+            .then(rfcLength => {
+                // build a set of all the RFC's indexes
+                var rfcSet = new Set([...Array(rfcLength).keys()]);
+                // remove used indexes (so we don't "add" the rfc back to the deck of rfcs)
+                history.forEach(x => rfcSet.delete(x.index));
+                // pick a random index for the Set of RFC Indexes...
+                var randomIndex = Math.floor(Math.random() * rfcSet.size)
+                // get the random rfc index
+                var randomRfcIndex = [...rfcSet][randomIndex];
+                return randomRfcIndex;
+            });
+
+        // lookup document number (rfc number isn't always in the document path)
+        var rfcDocument = await fetch(rfcListUrl(randomRfcIndex))
+            .then(response => {
+                return response.json();
+            })
+            .then(json => {
+                return json.objects[0].doc;
+            });
+
+        var rfcNumber = await fetch(rfcDocUrl(rfcDocument))
+            .then(response => {
+                return response.json();
+            })
+            .then(json => {
+                return parseInt(json.rfc);
+            });
+
+        // save the rfc information for today
+        save({
+            number: rfcNumber,
+            index: randomRfcIndex,
+            date: todayFormated
+        });
+
+        return rfcNumber;
+    };
 };
 
 
