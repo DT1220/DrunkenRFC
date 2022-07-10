324db1e5e8100065a96dc3f220070f7306580e5e
Royce Strange <roycest@swbell.net>
2022-07-10 15:50:41 -0500

    display history elements on history.html

--- assets/js/main-history.js
+++ assets/js/main-history.js
@@ -1,8 +1,22 @@
 // elements
-// todo....
+var returnButtonEl = $("#returnButton");
+var historyListEl = $("#historyList");
 
 // load from history in localstorage and display
-// todo....
+var displayHistory = function (parentEl) {
+    // load from history
+    var history = load();
+    // for testing puposes:
+    // var history = [{ number: 1, date: "08/08/2000" }, { number: 1, date: "08/08/2000" }, { number: 1, date: "08/08/2000" }];
+
+    // loop through history and display on page
+    history.forEach(h => {
+        parentEl.append(
+            $("<li>")
+                .addClass("p-2 m-3 border-4 border-gray-700")
+                .text(h.date + ": RFC " + h.number))
+    });
+};
 
 // button handler to return to start page
 // todo....
@@ -13,7 +27,7 @@ var main = function () {
     // todo....
     console.log("todo: main");
     // function calls to load/display conetent
-    // todo....
+    displayHistory(historyListEl);
 };
 
 // start
