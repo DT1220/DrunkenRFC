ae4f996d0f66fca0ce4a5ecba0a9ad6afc7d32c1
Daniel <danieltang@Daniels-MBP.lan>
2022-07-11 18:55:49 -0500

    github api code

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # DrunkenRFC
-Project1
+Project1 (Royce Strange, Chelsea Hernandez, and Daniel Tang)
+
+Be sure to inlclude:
+
+a brief description of the website and its use case
+
+The purpose for our project "The Druken RFC" is to pick a random RFC to get information about every day
+   As I user:
+   I WANT to broaden my developer knowledge by researching a random RFC every day. By doing so, I can be updated on any and all technical developments happening in the world. I never know what random fact can make or break my code and the more I know the better. 
+
+Link to History and Index:
+file:///Users/danieltang/DrunkenRFC/history.html
+file:///Users/danieltang/DrunkenRFC/index.html
+
+a picture of the website
+
--- assets/js/api-github.js
+++ assets/js/api-github.js
@@ -1,8 +1,51 @@
 // TODO: flesh out the function below to dyn create elements under parentEl, 
 // the function should return the promise chain built off your fetch call
-// displayGithub :: (parentEl : Jquery-Element) && (rfcNumber: Int) -> Promise
+//displayGithub :: (parentEl : Jquery-Element) && (rfcNumber: Int) -> Promise
+
+const GITHUBURL = "https://api.github.com/users/DT1220/repos";
+
+async function getUserRepos(){
+    const response = await fetch(GITHUBURL);
+    const displayGithub = await response.json();
+    return displayGithub;
+}
+var response = fetch ("https://api.github.com/users/DT1220/repos");
+console.log(response);
+
+fetch ("https://api.github.com/users/DT1220/repos").then(function(response){
+    console.log("inside",response);
+});
+console.log("outside");
+
+$.ajax("https://api.github.com/users/DT1220/repos").done(function(data){
+    console.log(data);
+});
+
+var getUserRepos = function(user){
+    var apiURL = "https://api.github.com/users/" + user + "/repos";
+
+    fetch(apiURL).then(function(response){
+        response.json().then(function(data){
+            console.log(data);
+        });
+    });
+};
+
 var displayGithub = function (parentEl, rfcNumber) {
     console.log("todo: display Github");
     // don't return this dummy promise...
-    return new Promise((resolve, reject) => {"dummy"});
+    function listrepos(username, listelement){
+        return new Promise((resolve, reject) => {
+            repocount = 0;
+            repourl = "https://api.github.com/users/" + username + "/repo"
+            fetch(apiurl)
+
+            
+        });
+
+    }
+
+
 };
+
+//return todayRFC.number;
