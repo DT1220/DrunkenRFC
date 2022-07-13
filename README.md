52b1fd38d3b778c3d5e5b59b637ae593457809bc
Daniel <danieltang@Daniels-MacBook-Pro.local>
2022-07-13 13:21:51 -0500

    github api updated code

--- README.md
+++ README.md
@@ -7,11 +7,12 @@ a brief description of the website and its use case
 
 The purpose for our project "The Druken RFC" is to pick a random RFC to get information about every day
    As I user:
-   I WANT to broaden my developer knowledge by researching a random RFC every day. By doing so, I can be updated on any and all technical developments happening in the world. I never know what random fact can make or break my code and the more I know the better. 
+   I WANT to broaden my developer knowledge by researching a random RFC every day. By doing so, I can be updated on any and all technical developments happening in the world. I never know what random fact can make or break my code and the more I know the better. This website is targetted towards eager minded individuals who want to learn something new everyday. 
 
 Link to History and Index:
 file:///Users/danieltang/DrunkenRFC/history.html
 file:///Users/danieltang/DrunkenRFC/index.html
+
 
 a picture of the website
 
--- assets/js/api-github.js
+++ assets/js/api-github.js
@@ -2,28 +2,40 @@ var displayGithub = function (parentEl, rfcNumber) {
     var rfcNumber = 4122;
     console.log("rfcNumber passed in: " + rfcNumber);
     var apiURL = "https://api.github.com/search/repositories?q=rfc" + rfcNumber + "&per_page=5";
+
     return fetch(apiURL)
         .then(function (response) {
             return response.json();
         })
         .then(function (json) {
-            console.log(json.items);
-            //...something
+            //console.log(json.items);
+            //let list = localStorage.getItem('json.items');
+            //const printlist = function(){
+            //    localStorage.setItem('list', JSON.stringify(json.items))
+            //}
+
             var data = [];
             for (var i = 0; i < json.items.length; i++) {
+                //items += <li> ${json[i]}</li>
                 data.push({
                    url: json.items[i].html_url,
                    name: json.items[i].name
+
                 });
             }
-            console.log(data);
+            //console.log(data);
+
+            console.log(i + "    " + data[i]);
             return data;
         })
         .then(function (data) {
+           
             var divEl = $("<div>")
-                .addClass("bg-slate-700")
-                .text(data[0].name + "   " + data[0].url);
-
+                //tailwind styling
+                .addClass("bg-slate-500 p-2 text-center text-white group-hover:bg-slate-600")
+                //displays onto the webpage
+                .text(data[1].name + "   " + data[1].url);
+                console.log(data);
             parentEl.empty();
             parentEl.append(divEl);
         });
