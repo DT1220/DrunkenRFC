39f3915a1bb99b7401cb36a4d5ff531da6fc1b78
Daniel <danieltang@Daniels-MBP.lan>
2022-07-13 19:41:32 -0500

    github api working

--- assets/js/api-github.js
+++ assets/js/api-github.js
@@ -1,6 +1,4 @@
 var displayGithub = function (parentEl, rfcNumber) {
-    var rfcNumber = 4122;
-    console.log("rfcNumber passed in: " + rfcNumber);
     var apiURL = "https://api.github.com/search/repositories?q=rfc" + rfcNumber + "&per_page=5";
 
     return fetch(apiURL)
@@ -8,36 +6,37 @@ var displayGithub = function (parentEl, rfcNumber) {
             return response.json();
         })
         .then(function (json) {
-            //console.log(json.items);
-            //let list = localStorage.getItem('json.items');
-            //const printlist = function(){
-            //    localStorage.setItem('list', JSON.stringify(json.items))
-            //}
-
             var data = [];
             for (var i = 0; i < json.items.length; i++) {
-                //items += <li> ${json[i]}</li>
                 data.push({
-                   url: json.items[i].html_url,
-                   name: json.items[i].name
+                    url: json.items[i].html_url,
+                    name: json.items[i].name
 
                 });
             }
-            //console.log(data);
 
             console.log(i + "    " + data[i]);
             return data;
         })
         .then(function (data) {
-           
-            var divEl = $("<div>")
-                //tailwind styling
-                .addClass("bg-slate-500 p-2 text-center text-white group-hover:bg-slate-600")
-                //displays onto the webpage
-                .text(data[1].name + "   " + data[1].url);
-                console.log(data);
             parentEl.empty();
-            parentEl.append(divEl);
+
+            for (var i = 0; i < data.length; i++) {
+                var divEl = $("<div>")
+                    //tailwind styling
+                    .addClass("bg-slate-500 p-2 text-center text-white group-hover:bg-slate-600")
+                    //displays onto the webpage
+                    .text(data[i].name + "   " + data[i].url);
+                parentEl.append(divEl);
+            }
+            if (data.length === 0) {
+                var divEl = $("<div>")
+                    //tailwind styling
+                    .addClass("bg-slate-500 p-2 text-center text-white group-hover:bg-slate-600")
+                    //displays onto the webpage
+                    .text("No repositories for this RFC on Github");
+                parentEl.append(divEl);
+            }
         });
 
 };
--- assets/js/saveload.js
+++ assets/js/saveload.js
@@ -1,92 +1,20 @@
-// save :: rfcObj : { number: Int, title: string, abstract: string } -> undefined
-var save = function(rfcObj) {
-    var myObject = [];
-    var myJSON = JSON.stringify(myObject);
-   // localStorage = 
-    console.log(myObject);
+
+
+// load up saved history from localstorage
+var load = function () {
+    var history = localStorage.getItem("rfchistory");
+    return JSON.parse(history) || [];
 };
+// save and object to localstorage
+var save = function (item) {
+    var history = load();
+    var x = [item].concat(history);
+    localStorage.setItem("rfchistory", JSON.stringify(x));
 
 
-// load :: () -> [{rfcObj}]
-var load = function() {
-    console.log("  ");
-    return [load];
 
-
-       function load() {
-        if (localStorage.getItem("Load") != null) {
-            var LOAD = JSON.parse(localStorage.getItem("Load"));
-    
-            for (var i = 0; i < LOAD.length; i++) {
-                var LOAD = LOAD[i];
-                newLOADItem(LOAD.task, LOAD.completed);
-                load();
-            }
-        }
-    } 
+    // load() the currently saved values (that is an array) and assign it to a variable.
+    // make a new array where the new item is in front of the elements of the loaded array (hint: https://stackoverflow.com/questions/3975170/javascript-how-to-join-combine-two-arrays-to-concatenate-into-one-array)
+    // convert that to JSON
+    // finally store that to localstorage (and return nothing)
 };
-
-$(document).ready(function () {
-    // saveBtn click listener 
-    $(".saveBtn").on("click", function () {
-        // Get nearby values of the description in JQuery
-        var text = $(this).siblings(".description").val();
-        // Save text in local storage
-        localStorage.setItem(save);
-    })
-}
-
-
-
-
-/* var timeEntries = []; // initialize list of log entries
-
-getTimeEntries(); // See if there are entries in localstorage and load them
-
-$(".saveBtn").click(saveClick); // Set event handler for all save buttons
-
-const timeEntriesName = "workDaySchedulerList"; // name used for localstorage
-const firstEntry = 9; // first displayed time block, relative to hourMap (9AM)
-const lastEntry = 17; // last display time block, relative to hourMap (5PM)
-const hourMap = ["12AM","1AM","2AM","3AM","4AM","5AM","6AM","7AM","8AM","9AM","10AM","11AM","12PM",
-                "1PM","2PM","3PM","4PM","5PM","6PM","7PM","8PM","9PM","10PM","11PM"]; // map of military hours
-
-// onClick event for all buttons
-function saveClick() {
-    var hourBlock = $(this).val(); // get which hour block we're in from button's value
-    var entryFound = false;
-    var newEntryIndex = timeEntries.length; // where in the timeEntries array the new object goes
-    // create new timeEntries object
-    var newEntry = {day: currentDate, time: hourBlock, text: $("#text"+hourBlock).val()}; 
-
-
-        // check the timeEntries array to see if there is already an entry for this hour
-    for (let i=0; i<timeEntries.length; i++) {
-        if (timeEntries[i].day == currentDate) {
-            if (timeEntries[i].time == hourBlock) {
-                timeEntries[i].text = newEntry.text; // If entry already exists, just update text
-                entryFound = true; // entry already exists
-                break;
-            }
-            // entry does not exist - insert it when you reach the first hour that is greter
-            else if (timeGreater(timeEntries[i].time, hourBlock)) {
-                newEntryIndex = i;
-                break;
-            }
-        }
-        // no entries exist for current day - insert when you reach first day that is greater
-        else if (timeEntries[i].day > currentDate) {
-            newEntryIndex = i;
-            break;
-        }
-    }
-
-    // If the entry didn't already exist, add it to the array in the appropriate place
-    if (!entryFound) {
-        timeEntries.splice(newEntryIndex, 0, newEntry);
-    }
-
-    // store in local storage
-    localStorage.setItem(timeEntriesName, JSON.stringify(timeEntries));
-}
-    */)
