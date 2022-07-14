e06b79220b943161c00b01acdc29df146b32aff7
Royce Strange <roycest@swbell.net>
2022-07-14 10:52:22 -0500

    make github info fancier

--- assets/js/api-github.js
+++ assets/js/api-github.js
@@ -1,4 +1,5 @@
 var displayGithub = function (parentEl, rfcNumber) {
+    var rfcNumber = 4122;
     var apiURL = "https://api.github.com/search/repositories?q=rfc" + rfcNumber + "&per_page=5";
 
     return fetch(apiURL)
@@ -15,28 +16,53 @@ var displayGithub = function (parentEl, rfcNumber) {
                 });
             }
 
-            console.log(i + "    " + data[i]);
-            return data;
+            return {
+                total: json.total_count,
+                repos: data
+            };
         })
         .then(function (data) {
-            parentEl.empty();
 
-            for (var i = 0; i < data.length; i++) {
-                var divEl = $("<div>")
+            var githubContainerEl = $("<div>")
+                .addClass("h-full flex flex-col");
+
+            var titleEl = $("<h2>")
+                //tailwind styling
+                .addClass("bg-slate-500 p-2 text-center text-white")
+                //displays onto the webpage
+                .text("Related Repos On GitHub:");
+            githubContainerEl.append(titleEl);
+
+            for (var i = 0; i < data.repos.length; i++) {
+                var aEl = $("<a>")
                     //tailwind styling
-                    .addClass("bg-slate-500 p-2 text-center text-white group-hover:bg-slate-600")
+                    .addClass("bg-slate-300 p-2 text-center border-2 border-slate-500 hover:bg-slate-400")
+                    .attr("href", data.repos[i].url)
+                    .attr("target", target = "_blank")
                     //displays onto the webpage
-                    .text(data[i].name + "   " + data[i].url);
-                parentEl.append(divEl);
+                    .text(data.repos[i].name);
+                githubContainerEl.append(aEl);
             }
-            if (data.length === 0) {
-                var divEl = $("<div>")
+            if (data.repos.length === 0) {
+                var emptyEl = $("<div>")
                     //tailwind styling
-                    .addClass("bg-slate-500 p-2 text-center text-white group-hover:bg-slate-600")
+                    .addClass("bg-slate-300 p-2 text-center")
                     //displays onto the webpage
-                    .text("No repositories for this RFC on Github");
-                parentEl.append(divEl);
+                    .text("No Repos for this RFC found on Github");
+                githubContainerEl.append(emptyEl);
             }
+            else if (data.total > data.repos.length) {
+                var moreEl = $("<a>")
+                    .addClass("bg-slate-500 p-2 text-center text-white hover:bg-slate-600")
+                    .attr("href", "https://github.com/search?q=rfc" + rfcNumber)
+                    .attr("target", target = "_blank")
+                    .text("See more on GitHub");
+                githubContainerEl.append(moreEl);
+            }
+
+
+            parentEl.empty();
+            parentEl.append(githubContainerEl);
         });
 
 };
